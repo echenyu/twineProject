f5603ccb2e1236dc6bb1852c5919d0a20f8dc244
Eric Yu (Youth) <echenyu@fb.com>
2019-05-30 16:44:05 -0700

    [3] Create fetcher for Goals data

---
+++ app/src/main/java/com/echenyuapps/twineproject/data/GoalModelFetcher.java
@@ -0,0 +1,30 @@
+package com.echenyuapps.twineproject.data;
+
+import com.echenyuapps.twineproject.model.GoalModel;
+
+import java.util.ArrayList;
+
+import static com.echenyuapps.twineproject.model.GoalModel.Status.*;
+
+public class GoalModelFetcher {
+
+  // Todo: Convert into a ListenableFuture for asynchronous fetching
+  public static ArrayList<GoalModel> fetchGoals() {
+    ArrayList<GoalModel> goalModels = new ArrayList<>();
+    GoalModel goalModel1 = new GoalModel("Rainy Ian", PAUSED);
+    GoalModel goalModel2 = new GoalModel("Goal Name", PAUSED);
+    GoalModel goalModel3 = new GoalModel("Trip To Hawaii", PAUSED);
+    GoalModel goalModel4 = new GoalModel("Kids", PAUSED);
+    GoalModel goalModel5 = new GoalModel("Vacation", PAUSED);
+    GoalModel goalModel6 = new GoalModel("Vacation", PAUSED);
+
+    goalModels.add(goalModel1);
+    goalModels.add(goalModel2);
+    goalModels.add(goalModel3);
+    goalModels.add(goalModel4);
+    goalModels.add(goalModel5);
+    goalModels.add(goalModel6);
+
+    return goalModels;
+  }
+}
